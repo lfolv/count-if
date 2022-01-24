6f777b354ecbc3ee49d0b495b56c4486138deb5d
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2022-01-24 14:57:23 -0300

    Create countIf function

--- count-if.js
+++ count-if.js
@@ -1,5 +1,11 @@
 function countIf(list, fn) {
-  return 0
+  return list.reduce((count, value) => {
+    if (fn(value)) {
+      return count + 1
+    }
+
+    return count
+  }, 0)
 }
 
 module.exports = countIf
