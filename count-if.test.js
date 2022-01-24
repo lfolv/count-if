2f567025a988f4d098a0f460b80d34110a83e216
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2022-01-24 15:00:50 -0300

    Setting Up prettier

--- count-if.test.js
+++ count-if.test.js
@@ -1,15 +1,20 @@
-const countIf = require('./count-if')
+const countIf = require("./count-if");
 
-describe('countIf', () => {
-  test('with an empty list', () => {
-    expect(countIf([], a => a === '' )).toBe(0)
-  })
+describe("countIf", () => {
+  test("with an empty list", () => {
+    expect(countIf([], (a) => a === "")).toBe(0);
+  });
 
-  test ('with an list with values', () => {
-    expect(countIf([1, 2, 3, 4, 5], a => a >= 3)).toBe(3)
-  })
+  test("with an list with values", () => {
+    expect(countIf([1, 2, 3, 4, 5], (a) => a >= 3)).toBe(3);
+  });
 
-  test('with another list of values', () => {
-    expect(countIf(['Steve Rogers', 'Steve Strange', 'Tony Stark', 'Peter Parker'], a => a.startsWith('S'))).toBe(2)
-  })
-})
+  test("with another list of values", () => {
+    expect(
+      countIf(
+        ["Steve Rogers", "Steve Strange", "Tony Stark", "Peter Parker"],
+        (a) => a.startsWith("S")
+      )
+    ).toBe(2);
+  });
+});
